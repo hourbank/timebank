11eb790ef39fe2472e8a493935db0844120c31d0
Lyn Muldrow <lyn.muldrow@gmail.com>
2015-05-11 14:59:35 -0700

    working home page no links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   # Root route -- this is temporary until we have better starting point
-  root to: "service_requests#index" # change this once we have some better page to start
+  root to: "site#index" # change this once we have some better page to start
   
   # User model & route generated via Devise gem
   devise_for :users
