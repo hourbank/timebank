2a9c8e75b90ae939bdc2315d54dd167e8177fb60
Annie Pennell <annie@anniepennell.com>
2015-05-11 14:41:27 -0700

    User can sign up and log in and log out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_filter :configure_devise_params, if: :devise_controller?
+    def configure_devise_params
+      devise_parameter_sanitizer.for(:sign_up) do |u|
+        u.permit(:first_name, :last_name, :email, :password, :password_confirmation, :phone, :services_offered, :city, :zipcode)
+      end
+    end
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
