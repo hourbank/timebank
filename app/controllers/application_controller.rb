689587ccb28a3dcd1b5c27597663af29a652d829
Annie Pennell <annie@anniepennell.com>
2015-05-12 13:45:05 -0700

    got user views working, added to temp navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  # allow Devise to add additional user sign up fields
   before_filter :configure_devise_params, if: :devise_controller?
     def configure_devise_params
       devise_parameter_sanitizer.for(:sign_up) do |u|
@@ -6,9 +11,9 @@ class ApplicationController < ActionController::Base
       end
     end
 
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+    def after_sign_in_path_for(resource)
+      '/service_requests'
+    end
 
   def send_sms_to(user,message)
   	# Expecting that user parameter is the OBJECT of the user to receive the message
--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,19 @@ Rails.application.routes.draw do
   resources :service_requests
 
   # Manual routes for all the rest
-  get "/users", to: "users#index" # Show all users is not part of Devise set of Routes
   get "/about", to: "site#about"
   get "/contact", to: "site#contact"
 
+  # show all users
+  get "/users", to: "users#index" 
+
+  # show current user's private account page
   get "/users/account", to: "users#account"
+
+  # show public profile page of specific user
+  get "/users/:id", to: "users#show", as: "user"
+
   # Also need post/get routes for edit view of users#account
+
   
 end
