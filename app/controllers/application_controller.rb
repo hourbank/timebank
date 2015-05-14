24fb7e90583a3699e4ebcf8b56a5a00bc2d78cf6
Steven Gordon <steven@SRG-MBP.local>
2015-05-14 10:25:29 -0700

    Updating Application controller for real Twilio API key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,14 +26,24 @@ class ApplicationController < ActionController::Base
     message = "Hello "+user.first_name+"! "+message
 
   	# This is the Twillio account information (SID and auth_token for whichever account we are using)
-  	account_sid = 'ACd9d81b036bad423457caf7ec9f506227' 
-  	auth_token = '2e9b24ce85e18f2e6773043efe25bca8'
+    # New real SID
+    account_sid = 'AC91d1e215d5a331ce1abc7decf99384cd' 
+  	
+    # Old trial SID
+    #account_sid = 'ACd9d81b036bad423457caf7ec9f506227' 
+
+    # NEED TO HIDE AUTHORIZATION TOKEN
+
+    auth_token = ENV["api_authorization_token"]
+
+    # Old trial authorization token:
+    #  	auth_token = '2e9b24ce85e18f2e6773043efe25bca8'
 
 	  @client = Twilio::REST::Client.new account_sid, auth_token
 
 	  message = @client.account.messages.create(
 		 :body => message,
 		 :to => "+1" + phone,
-		 :from => "+14155992671")
+		 :from => "+16503895939")
   end
 end
