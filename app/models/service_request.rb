303a37d9350d29680cf49c35077627622392de6c
Steven Gordon <steven@SRG-MBP.local>
2015-05-14 23:59:11 -0700

    Removed validation to allow 0 in estimated and final hours

--- app/models/service_request.rb
+++ app/models/service_request.rb
@@ -1,8 +1,9 @@
 class ServiceRequest < ActiveRecord::Base
   belongs_to :recipient, class_name: "User", foreign_key: "requested_by_id"
 
+  # DO NOT USE THE VALIDATIO BELOW -- AS WRITTEN IT CRASHES EXCHANGES
   # validates estimated hours is a number and is greater than 0
-  validates :estimated_hours, :numericality => {:only_integer => true, :greater_than => 0}
+  #validates :estimated_hours, :numericality => {:only_integer => true, :greater_than => 0}
   # validates :estimated_hours, :numericality => {:greater_than => 0}
 
   # Allow access to all these attributes
