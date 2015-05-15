07141736e41be2e5304d5ec9dee7425dd700a4ac
Steven Gordon <steven@SRG-MBP.local>
2015-05-15 08:11:10 -0700

    Fixed database query results to be in appropriate order. Also text changes on User Index.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,20 +2,20 @@ class UsersController < ApplicationController
 before_action :logged_in?
 
 	def index
-		@all_users = User.all
+		@all_users = User.all.order(:first_name)
 	end
 
 	def show
 		@user = User.find(params[:id])
 		@current_user = current_user
-		@requests = ServiceRequest.all
+		@requests = ServiceRequest.all.order(created_at: :desc)
 	end
 
 	def account
 		#Generate the account dashboard page for the currently logged-in user
 		@user = current_user
-		@requests = ServiceRequest.all
-		@exchanges= Exchange.all
+		@requests = ServiceRequest.all.order(created_at: :desc)
+		@exchanges= Exchange.all.order(created_at: :desc)
 	end
 
 	private
