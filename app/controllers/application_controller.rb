ed73401cc765ef336696638e6933c8a959190d51
Annie Pennell <annie@anniepennell.com>
2015-05-14 17:59:52 -0700

    changed redirect to dashboard after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
     end
 
     def after_sign_in_path_for(resource)
-      '/service_requests'
+      '/users/account'
     end
 
   # Need the Twilio Gem to send SMS messages
