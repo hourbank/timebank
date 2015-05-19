f0f3b16a35415462e1a25c7f0942856b9d9f1916
Steven Gordon <steven@SRG-MBP.local>
2015-05-19 16:36:04 -0700

    Fixed 404 css errors with Mike

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,10 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+
+Rails.application.config.assets.precompile += %w( font-awesome.min.css )
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
